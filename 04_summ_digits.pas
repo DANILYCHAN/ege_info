1a8c193f56ff0fda4ba1786074e34345abfcee5f
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-11 19:15:13 +0300

    Add files via upload

---
+++ 04_summ_digits.pas
@@ -0,0 +1,12 @@
+﻿Program sum_digits;
+Var
+  number,summ:integer;
+BEGIN
+ Read(number);
+ summ := 0;
+ While number > 0 do begin
+   summ := summ + (number mod 10);
+   number := number div 10;
+ end;
+ Writeln(summ); 
+END.
