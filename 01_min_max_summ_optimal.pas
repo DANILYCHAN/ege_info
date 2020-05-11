a9912bb3ab4c7424e1b4ff8a14fcf142a16550aa
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-11 18:39:04 +0300

    Add files via upload

---
+++ 01_min_max_summ_optimal.pas
@@ -0,0 +1,20 @@
+﻿Program z1;
+const N=1000;
+var
+  a: integer;
+  i, min, k, max, s: integer;
+BEGIN
+  readln(k);
+  min := 10001;
+  max := -1;
+  s := 0;
+  for i := 1 to k do begin 
+    readln(a);
+    if a < min then min := a;
+    if a > max then max := a;
+    s := s+a;
+  end;
+  Writeln(min);
+  Writeln(max);
+  Writeln(s);
+END.
---
+++ 02_combinatorika.pas
@@ -0,0 +1,22 @@
+﻿Program z1;
+
+ Function Factorial (n:integer): integer;
+  var
+   i,k: integer;
+  begin
+    k := 1;
+    for i := 1 to n do
+      k := k*i;
+    Factorial := k;
+  end;
+  
+ Function Combination (n, k: integer): integer;
+  begin
+    Combination := Factorial(n) div Factorial(n-k) div Factorial(k);
+  end;
+ 
+ 
+BEGIN
+ Writeln(Factorial(5));
+ Writeln(Combination(4, 2));
+end.
