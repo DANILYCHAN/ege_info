aa2542759c05191ba3a06cfe26ee0e22be9b9046
Dima Mikhalchenko <mixal4enko@gmail.com>
2020-05-11 19:21:56 +0300

    optimize memory, clarify varnames

--- 03_summ_even_odd.pas
+++ 03_summ_even_odd.pas
@@ -1,16 +1,19 @@
 ﻿Program summ_even_odd;
 const N=6;
 Var
-  a: array [1..N] of integer;
-  i,e,o:integer;
+  a, i, summ_evens, summ_odds :integer;
 BEGIN
-  For i := 1 to N do readln(a[i]);
-  e:=0;
-  o:=0;
+  summ_evens := 0;
+  summ_odds := 0;
+
   For i := 1 to N do begin
-    if a[i] mod 2 = 0 then e := e + a[i]
-    else o := o + a[i];
+    readln(a);
+    if a mod 2 = 0 then
+      summ_evens := summ_evens + a;
+    else
+      summ_odds := summ_odds + a;
   end;
-  Writeln('Summ of evens ', e);
-  Writeln('Sum of odds ', o);
+  Writeln('Summ of evens ', summ_evens);
+  Writeln('Sum of odds ', summ_odds);
 END.
+
