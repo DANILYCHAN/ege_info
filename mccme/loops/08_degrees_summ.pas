fba7a76ef62839c29d94f4e6ac78f8e07c414f85
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 21:02:17 +0300

    Create 08_degrees_summ.pas

---
+++ mccme/loops/08_degrees_summ.pas
@@ -0,0 +1,19 @@
+﻿Program degrees_summ;
+ 
+   Function Degree(a, n: integer): integer;
+   Var
+     i,k: integer;
+   begin
+     k := 1;
+     For i := 1 to n do
+       k := k * a;
+     Degree := k;
+   end;
+   
+  Var i, n, summ: integer;
+  BEGIN
+    Read(n);
+    For i := 0 to n do
+      summ := summ + Degree(2, i);
+    Writeln(summ);
+  END.
