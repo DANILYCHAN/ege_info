8dc2777d868c0512be302450ed5120502fe53d7a
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 20:30:54 +0300

    Create 03_number_of_combinations.pas

---
+++ mccme/loops/03_number_of_combinations.pas
@@ -0,0 +1,23 @@
+﻿Program Number_of_combinations;
+
+ Function Factorial (n:integer): integer;
+  var
+   i,k: integer;
+  begin
+    k := 1;
+    for i := 1 to n do
+      k := k*i;
+    Factorial := k;
+  end;
+  
+ Function Combination (n, k: integer): integer;
+  begin
+    Combination := Factorial(n) div Factorial(n-k) div Factorial(k);
+  end;
+ 
+ Var n,k: integer;
+ 
+BEGIN
+ Readln(n, k);
+ Writeln(Combination(n, k));
+end.
