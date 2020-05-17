110ae2d954025b2d5d24ec5ee2032934e51ecbb7
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 20:57:49 +0300

    Create 07_summ_2.pas

---
+++ mccme/loops/07_summ_2.pas
@@ -0,0 +1,12 @@
+﻿Program summ_2;
+Var 
+  i, n: integer;
+  summ: real;
+BEGIN
+  Read(n);
+  summ := 1;
+  For i := 1 to n do
+    summ := summ + (power((-1),i)/(2*i+1));
+  Writeln(4*summ);
+  
+END.
