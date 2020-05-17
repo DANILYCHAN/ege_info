99241394825ddf8dda3a31baab5ab3f08c7ef2bb
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 20:43:01 +0300

    Create 05_geometric_progression.pas

---
+++ mccme/loops/05_geometric_progression.pas
@@ -0,0 +1,23 @@
+﻿Program geometric_progression;
+
+  Function Degree(a, n: integer): integer;
+   Var
+     i,k: integer;
+   begin
+     k := 1;
+     For i := 1 to n do
+       k := k * a;
+     Degree := k;
+   end;
+   
+   Var
+     a, n, j, summ: integer;
+   
+  BEGIN
+    Read(a, n);
+    summ := 1;
+    For j := 1 to n do
+      summ := summ + Degree(a, j);
+    Writeln(summ);
+      
+  END.
