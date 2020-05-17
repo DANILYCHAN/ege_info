d014df8a651418acee2447427e1c1c44b780068c
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 23:33:53 +0300

    Create 10_summs_plus_minus_zeros.pas

---
+++ mccme/loops/Task_conditions/10_summs_plus_minus_zeros.pas
@@ -0,0 +1,15 @@
+﻿Program Summ_of_zeros;
+Var i, x, summ_of_zero, n, summ_plus, summ_minus: integer;
+BEGIN
+  Read(n);
+  summ_of_zero := 0;
+  summ_plus := 0;
+  summ_minus := 0;
+ For i := 1 to n do begin
+   Readln(x);
+   if x = 0 then summ_of_zero := summ_of_zero + 1 else
+     if x > 0 then summ_plus := summ_plus + 1 else
+       summ_minus := summ_minus + 1;
+   end;
+  Write(summ_of_zero,' ',summ_plus,' ',summ_minus);
+END.
