fdb1750d224a37b8e9eaec338884d14460bb391a
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-17 21:30:22 +0300

    Create 09_strange_summ.pas

---
+++ mccme/loops/09_strange_summ.pas
@@ -0,0 +1,21 @@
+﻿Program strange_summ;
+
+ Function Factorial (n:integer): integer;
+  var
+   i,k: integer;
+  begin
+    k := 1;
+    for i := 1 to n do
+      k := k*i;
+    Factorial := k;
+  end;
+  
+  Var summ: real;
+  i, n: integer;
+BEGIN
+ Readln(n);
+ summ := 0;
+ For i := 0 to n do
+   summ := summ + (1/Factorial(i));
+ Writeln(summ);
+END.
