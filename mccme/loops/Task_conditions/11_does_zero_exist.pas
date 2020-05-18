cf8aaca5f73a1456254b6986d47f18d09b407376
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-18 14:53:17 +0300

    Create 11_does_zero_exist.pas

---
+++ mccme/loops/Task_conditions/11_does_zero_exist.pas
@@ -0,0 +1,18 @@
+﻿{Задача №347. Ноль или не ноль. Проверьте, есть ли среди данных N чисел нули. Входные данные
+Вводится число N, а затем N чисел.
+Выходные данные
+Выведите YES, если среди введенных чисел есть хотя бы один нуль, или NO в противном случае.}
+Program does_zero_exist;
+Var a, n, i, zero: integer;
+BEGIN
+  Read(n);
+  zero := 0;
+  For i := 1 to n do begin
+    Read(a);
+    if (a = 0) then zero := 1;
+    end;
+  If (zero = 1) then 
+    Writeln('YES')
+  else Writeln('NO');
+END.
+
