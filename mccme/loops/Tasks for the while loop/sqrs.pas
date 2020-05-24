1a80a75f93781d7b797875864cdc88f0e5ad3e31
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-24 19:20:44 +0300

    Create sqrs.pas

---
+++ mccme/loops/Tasks for the while loop/sqrs.pas
@@ -0,0 +1,10 @@
+﻿Program sqrs; {Выведите все точные квадраты натуральных чисел, не превосходящие данного числа N.}
+Var i, n: integer;
+BEGIN
+  Read(n);
+  i := 1;
+  While sqr(i) < n do begin
+    Writeln(sqr(i));
+    i += 1;
+    end;
+END.
