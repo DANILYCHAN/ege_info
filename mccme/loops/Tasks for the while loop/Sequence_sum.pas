007d292d1c694bebb6f39b9677a529045d251d74
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-25 18:20:57 +0300

    Create Sequence_sum.pas

---
+++ mccme/loops/Tasks for the while loop/Sequence_sum.pas
@@ -0,0 +1,12 @@
+﻿Program Sequence_sum;{Определите сумму всех элементов последовательности, завершающейся числом 0.Числа, следующие за нулем, считывать не нужно.}
+Var 
+  A: array [1..10001] of integer;
+  i, k: integer;
+BEGIN
+  For i := 1 to 10001 do begin
+    Read(a[i]);
+    if a[i] <> 0 then k += a[i] else
+      break
+    end;
+    Writeln(k);
+END.
