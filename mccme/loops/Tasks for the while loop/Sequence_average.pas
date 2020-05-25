06394f09a055e7b04a793c322d9c8786eec91822
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-25 18:20:55 +0300

    Create Sequence_average.pas

---
+++ mccme/loops/Tasks for the while loop/Sequence_average.pas
@@ -0,0 +1,18 @@
+﻿Program Sequence_average;{Определите сумму всех элементов последовательности, завершающейся числом 0.Числа, следующие за нулем, считывать не нужно.}
+Var 
+  A: array [1..10001] of integer;
+  i: integer;
+  k, p: real;
+BEGIN
+  
+  For i := 1 to 10001 do begin
+    Read(a[i]);
+    if a[i] <> 0 then begin
+    k += a[i];
+    p += 1;
+    end;
+    if a[i] = 0 then
+      break
+    end;
+    Writeln(k/p);
+END.
