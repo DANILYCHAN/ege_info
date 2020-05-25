ec3a8acbe9279f3c0ee214cf706e21ea2d07bfdd
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-25 18:33:49 +0300

    Create Maximum_consistency.pas

---
+++ mccme/loops/Tasks for the while loop/Maximum_consistency.pas
@@ -0,0 +1,15 @@
+﻿Program Maximum_consistency;{Последовательность состоит из натуральных чисел и завершается числом 0. Определите значение наибольшего элемента последовательности.
+Числа, следующие за нулем, считывать не нужно.}
+Var 
+  A: array [1..10001] of integer;
+  i, max: integer;
+BEGIN
+  max := -1;
+  For i := 1 to 10001 do begin
+    Read(a[i]);
+    if a[i] > max then max := a[i];
+    if a[i] = 0 then
+      break
+    end;
+    Writeln(max);
+END.
