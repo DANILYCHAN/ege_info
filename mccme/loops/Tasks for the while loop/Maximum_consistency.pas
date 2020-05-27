fc29195b3c2b0b322b90c9d9eb7599475127b57b
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-27 18:03:16 +0300

    Убрал массив
    На самом деле на информатиксе с массивом показали OK на всех тестах.

--- mccme/loops/Tasks for the while loop/Maximum_consistency.pas
+++ mccme/loops/Tasks for the while loop/Maximum_consistency.pas
@@ -1,14 +1,13 @@
 ﻿Program Maximum_consistency;{Последовательность состоит из натуральных чисел и завершается числом 0. Определите значение наибольшего элемента последовательности.
 Числа, следующие за нулем, считывать не нужно.}
 Var 
-  A: array [1..10001] of integer;
-  i, max: integer;
+  a, i, max: integer;
 BEGIN
   max := -1;
   For i := 1 to 10001 do begin
-    Read(a[i]);
-    if a[i] > max then max := a[i];
-    if a[i] = 0 then
+    Read(a);
+    if a > max then max := a;
+    if a = 0 then
       break
     end;
     Writeln(max);
