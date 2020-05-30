a3797381e1ec43416c543e818ac47d085527a25d
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-30 20:21:08 +0300

    Create chislo_par_dayushie_chetnoe_v_proizvedeni.pas

---
+++ mccme/programs_for_counting_pairs_and_for_finding_highs/chislo_par_dayushie_chetnoe_v_proizvedeni.pas
@@ -0,0 +1,17 @@
+﻿Program chislo_par_dayushie_chetnoe_v_proizvedenii;
+Var 
+  number_even, number_odd, number_numbers, number_composition_even, number, i : integer;
+BEGIN
+  writeln('Количество чисел');
+  read(number_numbers);
+  for i:= 1 to number_numbers do begin
+    read(number);
+    if number mod 2 = 0 then begin
+      number_even += 1;
+    end else begin
+      number_odd += 1;
+    end;
+  end;
+  number_composition_even := ((sqr(number_even) - number_even) div 2) + (number_even * number_odd);
+  Writeln('Количество пар, дающих в произведении чётное число = ', number_composition_even);
+END.
