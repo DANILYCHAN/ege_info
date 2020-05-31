b2de86c049e18ae5627104c821963e4e65729938
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-31 22:00:19 +0300

    Create min_and_max_digits.pas

---
+++ mccme/loops/Tasks_analysis_digits_numbers/min_and_max_digits.pas
@@ -0,0 +1,42 @@
+﻿{   
+    Задача №116. Минимальная и максимальная цифры
+    Вход : число
+    Выход : минимальная и максимальная цифры числа
+}
+function MinDigit (n:longint):integer;
+
+VAR
+    min, digit : integer;
+begin
+    min := 11;
+    
+    while n > 0 do begin
+        digit := n mod 10;
+        if digit < min then
+            min := digit;
+        n := n div 10;
+    end;
+    MinDigit := min;
+end;
+
+function MaxDigit (n:longint):integer;
+
+VAR
+    max, digit : integer;
+begin
+    
+    while n > 0 do begin
+        digit := n mod 10;
+        if digit > max then
+            max := digit;
+    end;
+    MaxDigit := max;
+end;
+
+Var
+    n : integer;
+
+BEGIN
+    Read(n);
+    Write(MinDigit(n), ' ', MaxDigit(n));
+END.
