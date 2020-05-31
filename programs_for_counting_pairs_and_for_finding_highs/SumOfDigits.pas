9a2975b7b1dde7e2e35936dcb99ef647ce42a9b0
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-05-31 21:08:35 +0300

    Create SumOfDigits.pas

---
+++ programs_for_counting_pairs_and_for_finding_highs/SumOfDigits.pas
@@ -0,0 +1,31 @@
+﻿{
+    Задача №114. Сумма цифр числа
+    Вход : число
+    Выход : сумма цифр числа
+}
+function SumOfDigits (n:longint) : integer;
+
+VAR
+    summ_digits := 0;
+    
+begin
+    summ_digits := 0;
+        
+    while n > 0 do begin
+        summ_digits += n mod 10;
+        n := n div 10;
+    end;
+    SumOfDigits := summ_digits;
+end;
+
+VAR
+    n : integer;
+
+BEGIN
+  Read(n);
+  Writeln(SumOfDigits(n));
+END.   
+
+    
+  
+  
