0d8619962647c2e93dedc3e2f206dfbf8c332465
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-01 16:35:33 +0300

    Create reverse2.0.pas

---
+++ mccme/loops/Tasks_analysis_digits_numbers/reverse2.0.pas
@@ -0,0 +1,29 @@
+﻿{
+    Задача №118. Обращение числа
+    Ввод : число
+    Вывод : цифры данного числа в обратном порядке
+}
+function reverse (n:integer):integer;
+
+VAR
+    digit  : integer;
+    
+begin
+    digit := 0;
+
+    While n > 0 do begin
+        digit := digit * 10 + (n mod 10);
+        n := n div 10;
+    end;
+    
+    reverse := digit;
+end;
+
+Var 
+    n : integer;
+    
+BEGIN
+    Readln(n);
+    Writeln(reverse(n));
+END.
+
