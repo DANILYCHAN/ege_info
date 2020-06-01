ca02fcd55df12bda6b3814b4317e8aa965d38940
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-01 15:37:22 +0300

    Create binary_write_in_reverse.pas

---
+++ mccme/loops/Tasks_analysis_digits_numbers/binary_write_in_reverse.pas
@@ -0,0 +1,17 @@
+﻿{
+    Задача №117. Двоичная запись
+    Вход : число
+    Вывод : двоичная запись числа в обратном порядке
+}
+Program binary_write_in_reverse;
+Var
+    n : integer;
+BEGIN
+    Write('Введите число ');
+    Readln(n);
+    While n > 0 do begin
+        write(n mod 2);
+        n := n div 2;
+    end;
+END.
+    
