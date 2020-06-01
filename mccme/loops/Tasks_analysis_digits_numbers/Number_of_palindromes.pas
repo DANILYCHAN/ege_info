80408281586a419bb1fd63a3d67661dbbc1ea4ae
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-01 17:08:38 +0300

    Create Number_of_palindromes.pas

---
+++ mccme/loops/Tasks_analysis_digits_numbers/Number_of_palindromes.pas
@@ -0,0 +1,37 @@
+﻿{   Назовем число палиндромом, если оно не меняется при перестановке его цифр в обратном порядке.
+    Задача №119. Количество палиндромов
+    Ввод : число
+    Вывод : количество натуральных палиндромов, не превосходящих K
+}
+function reverse (n:integer):integer;
+
+VAR
+    digit  : integer;
+    
+begin
+    digit := 0;
+
+    While n > 0 do begin
+        digit := digit * 10 + (n mod 10);
+        n := n div 10;
+    end;
+    
+    reverse := digit;
+end;
+
+Var 
+    k, n, i : integer;
+    
+BEGIN
+    Read(k);
+    n := 0;
+  
+    For i := 1 to k do begin
+        if i = reverse(i) then begin
+            n += 1;
+        end;
+    end;
+    
+    write(n);
+END.
+
