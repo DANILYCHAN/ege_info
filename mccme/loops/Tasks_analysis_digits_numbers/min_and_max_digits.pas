c579826b3fe35c01f1c432d8f33e07936072f496
Dima Mikhalchenko <mixal4enko@gmail.com>
2020-06-01 21:26:45 +0300

    fix min max digits

--- mccme/loops/Tasks_analysis_digits_numbers/min_and_max_digits.pas
+++ mccme/loops/Tasks_analysis_digits_numbers/min_and_max_digits.pas
@@ -4,39 +4,41 @@
     Выход : минимальная и максимальная цифры числа
 }
 function MinDigit (n:longint):integer;
-
-VAR
+var
     min, digit : integer;
 begin
     min := 11;
     
-    while n > 0 do begin
+    repeat
         digit := n mod 10;
         if digit < min then
             min := digit;
         n := n div 10;
-    end;
+    until n = 0;
+
     MinDigit := min;
 end;
 
 function MaxDigit (n:longint):integer;
-
-VAR
+var
     max, digit : integer;
 begin
-    
-    while n > 0 do begin
+    max := -1;
+
+    repeat
         digit := n mod 10;
+        n := n div 10;
         if digit > max then
             max := digit;
-    end;
+    until n = 0;
+
     MaxDigit := max;
 end;
 
-Var
+VAR
     n : integer;
 
 BEGIN
     Read(n);
-    Write(MinDigit(n), ' ', MaxDigit(n));
+    Writeln(MinDigit(n), ' ', MaxDigit(n));
 END.
