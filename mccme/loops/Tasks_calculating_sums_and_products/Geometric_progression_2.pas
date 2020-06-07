845738714d435221d56df8bbf285367c711c3908
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-07 18:27:12 +0300

    Create Geometric_progression_2.pas

---
+++ mccme/loops/Tasks_calculating_sums_and_products/Geometric_progression_2.pas
@@ -0,0 +1,13 @@
+﻿Program Geometric_progression_2;
+VAR
+    a, n, k, summ, i : integer;
+BEGIN
+    Read(a, n);
+    k := 1;
+    For i := 0 to n do begin
+        summ += k;
+        k *= a;
+    end;
+    Writeln(summ);
+END.
+    
