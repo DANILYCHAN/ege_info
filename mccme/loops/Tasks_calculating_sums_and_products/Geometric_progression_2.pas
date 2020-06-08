bb4995a45d0d16f3aebe0a729c5c1031a1ce1953
Dima Mikhalchenko <mixal4enko@gmail.com>
2020-06-08 07:00:01 +0300

    add jedy-way task description

--- mccme/loops/Tasks_calculating_sums_and_products/Geometric_progression_2.pas
+++ mccme/loops/Tasks_calculating_sums_and_products/Geometric_progression_2.pas
@@ -1,4 +1,13 @@
-﻿Program Geometric_progression_2;
+{
+     Источник: informatics.mccme.ru, ???.
+     
+     Вход: 
+            a - основание геометрической прогрессии, 
+            n - номер последнего члена в сумме.
+     Выход: сумма прогрессии от первого члена до указанного последнего.
+}
+ 
+Program Geometric_progression_2;
 VAR
     a, n, k, summ, i : integer;
 BEGIN
