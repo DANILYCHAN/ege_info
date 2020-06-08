042ba21e4b51fb745e86ee24449de8c33bd7dbe9
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-08 15:13:18 +0300

    Create Maximum_consistency_2.pas

---
+++ mccme/loops/Tasks_for_the_while_loop/Maximum_consistency_2.pas
@@ -0,0 +1,26 @@
+﻿{
+    Источник : informatics.mccme.ru Задача №3068
+    
+    Вход:
+          a - последовательность чисел оканчивающаяся нулём.
+    Выход: максимальное число последовательности
+}
+
+Program Maximum_consistency;
+
+VAR 
+  a, max: integer;
+
+BEGIN
+    max := -1;
+    a := 1;
+    
+    While a <> 0 do begin
+        read(a);
+        if a > max then begin
+            max := a;
+        end;
+    end;
+  
+    Writeln(max);
+END.
