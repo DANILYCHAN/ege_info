08ae5761a31c6cad0fcf0ef0d019d4dd43d05f70
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-14 18:42:54 +0300

    Create max_summ_mod_U_any_var_A.pas

---
+++ summ_difference_mod_N/max_summ_mod_U_any_var_A.pas
@@ -0,0 +1,27 @@
+﻿{
+    Вычисление максимальной суммы делящейся на любое U.
+    Вход: число U, количество чисел, числа.
+    Вывод: максимальная суммы делящейся на любое U
+}
+Program max_summ_mod_U;
+    VAR
+        U, N, i, j, summ: Integer;
+        a: array[1..10001] of Integer;
+BEGIN
+    summ := -1;
+    Readln(U);
+    Readln(N);
+    for i := 1 to N do begin
+        read(a[i]);
+    end;
+    
+    for i := 1 to N - 1 do begin
+        for j := i + 1 to N do begin
+            if ((a[i] + a[j]) mod U = 0) and ((a[i] + a[j]) > summ) then begin
+                summ := a[i] + a[j];
+            end;
+        end;
+    end;
+    
+    Writeln(summ);
+end.
