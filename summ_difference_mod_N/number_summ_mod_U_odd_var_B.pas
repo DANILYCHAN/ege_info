2b5fb33fb515b738d8a576aa75004eb2cf45d37d
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-14 18:23:28 +0300

    Create number_summ_mod_U_odd_var_B.pas

---
+++ summ_difference_mod_N/number_summ_mod_U_odd_var_B.pas
@@ -0,0 +1,34 @@
+﻿{
+    Найти количество пар чисел делящихся на нечётное U.
+    Ввод: Число U, количество чисел, числа.
+    Вывод: количество пар чисел делящихся на нечётное U.
+}
+Program summ_mod_U_odd_var_B;
+VAR
+    U: Integer;{Число, на которое необходимо поделить}
+    N: Integer;{Количество чисел}
+    num: Integer;{Количество пар делящихся на U}
+    a: array[0..10000] of Integer;{Остатки от деления на U}
+    i, j, k: Integer;
+BEGIN
+    Readln(U);
+    Readln(N);
+    num := 0;
+    for i := 1 to (U - 1) do begin
+        a[i] := 0;
+    end;
+    
+    for i := 1 to N do begin
+        read(k);
+        j := k mod U;
+        a[j] += 1;
+    end;
+    
+    for i := 1 to (U - 1) div 2 do begin
+        num += a[i] * a[U - i];
+    end;
+    
+    num += (a[0] * (a[0] - 1)) div 2;
+    
+    Writeln(num);
+END.
