65fee9cc2658c4e629815063876a5a33657caa00
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-21 18:30:16 +0300

    Новые варианты 27 задач

---
+++ BUFER/RingBuf.pas
@@ -0,0 +1,65 @@
+﻿{
+  Найти максимальную сумму элементов, 
+  остоящих друг от друга на 3 позиции в потоке.
+}
+
+PROCEDURE Naive;
+{ Наивное решение: TIME: O(n), MEMORY: O(n) }
+CONST
+    SIZE = 8;
+    DELTA = 3;
+VAR
+    a: array[1..SIZE] of Integer = (15,6,3,7,2,8,9,65);
+    i: Integer;
+    sum : Integer;
+    max : Integer = -1;
+BEGIN
+  {
+    For i := 1 to 8 do begin
+        readln(a[i]);
+    end;
+  }
+    
+    For i := 1 to SIZE - DELTA do begin
+        sum := a[i] + a[i + DELTA];
+        if sum > max then max := sum;
+    end;
+    
+    WriteLn(max);
+END;
+
+Procedure ByRingBuf;
+{ 
+  input: поток натуральных чисел, оканчивающийся 0
+  output: список сумм элементов, отстоящих на DELTA 
+}
+    Const
+        DELTA = 3;
+    VAR
+        ring_buf : array[0..DELTA - 1] of Integer;  {!!!От нуля}
+        buf_ix : Integer;  {Указатель текущего значения}
+        cur_val : Integer = -1;
+        i : Integer;
+    BEGIN
+        { ВНИМАНИЕ! КОЛЬЦЕВОЙ БУФЕР ОТ НУЛЯ!!! }
+        For i := 0 to DELTA - 1 do begin 
+            read(ring_buf[i]);
+        end;
+        
+        buf_ix := 0;
+        
+        While cur_val <> 0 do begin
+            Read(cur_val);
+            If cur_val <> 0 then begin
+                Write(ring_buf[buf_ix] + cur_val, ' ');
+                ring_buf[buf_ix] := cur_val;
+                buf_ix := (buf_ix + 1) mod DELTA; {!!!ЗАКОЛЬЦОВКА}  
+            end;
+        end;
+        WriteLn;
+    END;
+        
+BEGIN { MAIN }
+    {Naive;}
+    ByRingBuf;    
+END.
---
+++ elements_spaced_apart_by_N/max_summ_numbers_in_3_Var_A.pas
@@ -0,0 +1,33 @@
+﻿{
+    Input : поток чисел оканчивающийся нулём
+    Output : максимальная сумма чисел, отстоящих друг от друга не менее чем на 3 элемента
+}
+
+Program max_summ_numbers_in_3_Var_A;
+
+const N = 10;
+
+VAR
+    max_summ : Integer; {Искомое значение}
+    a : array[1..N] of Integer;
+    i, j : Integer;
+    k : Integer;
+    
+BEGIN
+    for i := 1 to N do begin
+        Read(a[i]);
+    end;
+    
+    max_summ := -1;
+    k := 0;
+    for i := 1 to N do begin
+        for j := 3 to N - 3 do begin
+            k := a[i] + a[j];
+            if k > max_summ then begin
+                max_summ := k;
+            end;
+        end;
+    end;
+    
+    Writeln(max_summ);
+END.
