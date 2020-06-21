eb628111e6ae8f88ddd66962de0efd47c4426ae5
DANILYCHAN <65180476+DANILYCHAN@users.noreply.github.com>
2020-06-21 18:48:53 +0300

    Update max_summ_numbers_in_3_Var_A.pas

--- BUFER/max_summ_numbers_in_3_Var_A.pas
+++ BUFER/max_summ_numbers_in_3_Var_A.pas
@@ -6,7 +6,9 @@
 
 Program max_summ_numbers_in_3_Var_A;
 
-const N = 10;
+const 
+    N = 10;
+    DELTA = 3;
 
 VAR
     max_summ : Integer; {Искомое значение}
@@ -21,8 +23,8 @@ BEGIN
     
     max_summ := -1;
     k := 0;
-    for i := 1 to N do begin
-        for j := 3 to N - 3 do begin
+    for i := 1 to N - DELTA do begin
+        for j := DELTA to N do begin
             k := a[i] + a[j];
             if k > max_summ then begin
                 max_summ := k;
